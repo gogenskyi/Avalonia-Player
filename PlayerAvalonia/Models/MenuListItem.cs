466001b2e191f9644336f5af826f484547f28e3f
Gogenych <kpyvniuk@gmail.com>
2025-08-31 14:51:55 +0200

    Добавьте файлы проекта.

---
+++ PlayerAvalonia/Models/MenuListItem.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerAvalonia.Models
+{
+    public class MenuListItem
+    {
+        public string Name { get; set; }
+        public string Page { get; set;}
+        //public string Icon { get; set; }
+
+    }
+}
---
+++ PlayerAvalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using PlayerAvalonia.Models;
+
+namespace PlayerAvalonia.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public List<MenuListItem> Menu { get; } = new()
+        {
+            new MenuListItem
+            {
+                Name = "Home", Page = "MainPage"
+            },
+            new MenuListItem
+            {
+                Name = "Search", Page = "MainPage"
+            },
+            new MenuListItem
+            {
+                Name = "Library", Page = "MainPage"
+            },
+            new MenuListItem
+            {
+                Name = "Albums", Page = "Albums"
+            },
+            new MenuListItem
+            {
+                Name = "Artists", Page = "Artists" 
+            }
+        };
+
+        
+    }
+}
